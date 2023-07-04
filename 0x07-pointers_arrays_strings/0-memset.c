3af7066fa93ca7147a734396e50b26f90259f529
OloladeKajopinola <kololadeitunnu@gmail.com>
2023-07-04 03:46:38 -0700

    code correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,29 +1,32 @@
-
 /**
- * _strcat - concatenates two strings
- * @dest: string to append to
- * @src: string to add
+ * _memset - a function that fills
+ *         memory with a constant byte
  *
- * Return: a pointer to the resulting string
- */
-char *_strcat(char *dest, char *src)
+ * @s: input pointer to char type
+ *    represents the pointer to the
+ *    block of memory to fill
+ * @b: input variable of char type
+ *    represents the character to
+ *    fill s
+ * @n: unsigned int variable
+ *    the number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory
+ *         area @s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
 {
-	int i, j;
+	/**
+	 * declare an unsigned int
+	 * because we are storing a
+	 * value that will always be
+	 * non-negative (zero or positive)
+	*/
+	unsigned int i;
 
-	i = 0;
-	j = 0;
+	for (i = 0; i < n; i++)
+		s[i] = b;
 
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-
-	dest[i] = '\0';
-
-	return (dest);
+	return (s);
 }
