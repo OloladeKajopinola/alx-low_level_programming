97dc0444787192ffb0f22b0a90578bc4c09acc96
OloladeKajopinola <kololadeitunnu@gmail.com>
2023-07-31 06:41:43 -0700

    task 01 for  this task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len: Function that prints the number of elements in the list
+ * @h: Pointer to the head of the list
+ * Return: Number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0; /* to keep track of the nummber of nodes */
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
