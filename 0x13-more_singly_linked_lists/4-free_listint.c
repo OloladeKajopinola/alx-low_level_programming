9fa553660690e4fcb79be4836aecbba60aaf4d91
OloladeKajopinola <kololadeitunnu@gmail.com>
2023-07-31 23:12:12 -0700

    4th task betty complaint

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * free_listint - frees a linked list
@@ -8,10 +9,13 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head)
+	while (head != NULL)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		/* Store the current node to free it later */
+		temp = head;
+		/* Move head to the next node before freeing the current node */
+		head = head->next;
+		/* Free the current node */
+		free(temp);
 	}
 }
