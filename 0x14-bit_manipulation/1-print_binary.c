39934b4a48ac317669e5345a7a5d0eabdbe5e52f
OloladeKajopinola <kololadeitunnu@gmail.com>
2023-08-03 13:22:50 -0700

    task 1th printing binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,23 +6,31 @@
  */
 void print_binary(unsigned long int n)
 {
+	/* Create a mask ith only the most significant bit set */
 	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	/* To track leading zeros */
 	int flag = 0;
 
+	/* If n is 0, directly print 0 and return */
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
+	/* Iterate through each bit from MSB  to LSB */
 	while (mask != 0)
 	{
+		/* Check if the current bit is set (1) or not (0) */
 		if ((n & mask) != 0)
 		{
 			_putchar('1');
+			/* Mark that non-zero bit is found */
 			flag = 1;
 		}
 		else if (flag == 1)
+			/* Print leading zeros after the first non-zero bit */
 			_putchar('0');
+		/* Move to the next bit */
 		mask = mask >> 1;
 	}
 }
