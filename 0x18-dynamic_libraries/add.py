9713c5aa506f15105819d8c435c66be29e66e347
OloladeKajopinola <ololadeitunnu@gmail.com>
2023-09-22 09:38:42 -0700

    testing again to pass all checker

--- 0x18-dynamic_libraries/add.py
+++ 0x18-dynamic_libraries/add.py
@@ -3,3 +3,7 @@ mylib = ctypes.CDLL("./100-operations.so")
 a = 3
 b = 4
 print("{} + {} = {}".format(a, b, mylib.add(a, b)))
+print("{} - {} = {}".format(a, b, mylib.sub(a, b)))
+print("{} x {} = {}".format(a, b, mylib.mul(a, b)))
+print("{} / {} = {}".format(a, b, mylib.div(a, b)))
+print("{} % {} = {}".format(a, b, mylib.mod(a, b)))
